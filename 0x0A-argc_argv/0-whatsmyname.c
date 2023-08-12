a67625856a8f7c1a0add7f86d38b792572a69720
Hellen Maseko <masekohelen@gmail.com>
2023-08-11 23:58:02 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Returm: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
