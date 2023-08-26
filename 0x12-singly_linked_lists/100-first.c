63d6641514e442bee60afd09054fc3373b092c03
Hellen Maseko <masekohelen@gmail.com>
2023-08-26 00:46:10 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,11 +3,11 @@
 void ComeFirstbyCTC(void)__attribute__ ((constructor));
 
 /**
- * ComeFirst - Function that runs before the main function
+ * ComeFirstbyCTC - Function that runs before the main function
  */
 
 void ComeFirstbyCTC(void)
 {
-	printf("You're beat! and yet, you must allow, \n");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
