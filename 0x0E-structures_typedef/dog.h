946911a7d98337b8f66bfe299f7c9c5d3b08b449
Hellen Maseko <masekohelen@gmail.com>
2023-11-08 03:54:08 -0800

    4-new_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,5 +20,7 @@ void print_dog(struct dog *d);
 typedef struct dog dog_t;
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
+dog_t *new_dog(char *name, float age, char *owner);
+
 
 #endif
