964d157d56f81305807fe850fa64e5844ea19071
Hellen Maseko <masekohelen@gmail.com>
2023-11-21 06:32:21 -0800

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,8 +2,8 @@
 
 /**
  * print_listint - prints all the elements of a listint_t list
- * @h: a pointer to the head of the list_t list
- * Return: the number of nodes in the list_t list
+ * @h: a pointer to the head of the list
+ * Return: the number of nodes in the list
  */
 
 size_t print_listint(const listint_t *h)
