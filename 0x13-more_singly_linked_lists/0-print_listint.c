6c5193171560ac30873ae3d39dbc6a15ff5daf7e
Hellen Maseko <masekohelen@gmail.com>
2023-11-21 06:09:01 -0800

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,16 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
+	size_t count;
 
-	while (h)
+	if (h == NULL)
+		return (0);
+
+	for (count = 0; h != NULL; count++)
 	{
-		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
 	}
 
-	return (nodes);
+	return (count);
 }
